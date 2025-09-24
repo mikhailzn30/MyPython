ef9c734989647fd4ec421527afbc67befc1d43dc
mikhailzn30 <mikhailzn30@gmail.com>
2025-09-24 23:53:50 +0500

    int

--- integer/int.py
+++ integer/int.py
@@ -6,7 +6,15 @@ b = '321'
 b = int(b)
 print('b = ', b, type(b))
 
-
-num1 = input()
+# Ввод ЧИСЛА
+num1 = int(input())
 num2 = int(input())
-print('sum = ', num1 + num2)
+print('sum1 = ', num1 + num2)
+
+sum2 = int(input('num3 = ')) + int(input('num4 = '))
+print('sum2 = ', sum2)
+
+# Преобразование целого числа к строке
+str1 = str(123)
+print('str1 = ', str1, type(str1))
+
